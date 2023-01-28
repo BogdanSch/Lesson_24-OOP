4291d65ee1cee92b0391b4f2dabb2256b33ab8b5
Bohdan <bogmira777@gmail.com>
2023-01-28 10:54:37 +0100

    First commit

---
+++ test-oop-2-2023/MyCat.php
@@ -0,0 +1,31 @@
+<?php
+include "Cat.php";
+
+class MyCat extends Cat
+{
+    protected $name;
+
+    public function __construct($name = "", $age = 1)
+    {
+        parent::__construct($age);
+        $this->name = $name;
+    }
+
+    public function sleep()
+    {
+        echo "\nZzzzz...\n";
+    }
+
+    public function foo(Cat $obj)
+    {
+        parent::foo($obj);
+        echo $obj->name;
+    }
+}
+
+$my_cat = new MyCat("kitty", 5);
+
+echo $my_cat->getAge();
+$my_cat->sleep();
+
+$my_cat->foo(new MyCat("Sam", 8));
